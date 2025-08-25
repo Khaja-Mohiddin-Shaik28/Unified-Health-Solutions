30d85922a2eeb970c5555aecc838e83fb701c450
Khaja_Mohiddin_Shaik <khaja28mohiddin@gmail.com>
2025-08-25 19:11:27 +0530

    login api added with authentication and route verifying middleware

--- backend/controller/loginRegisterController.js
+++ backend/controller/loginRegisterController.js
@@ -52,8 +52,44 @@ const duplicateUserIdChecker = async (req, res) => {
   }
 };
 
+const login = async (req, res) => {
+  try {
+    const data = req.body;
+    // checking if user exists or not
+    const existingUserData = await User.findOne({ emailId: data.emailId });
+
+    // if emailId doesn't exist
+    if (!existingUserData) {
+      return res.status(404).json({ status: "User does not exist" });
+    }
+
+    // if emailId exist then we compare passwords
+    const validPassword = await bcrypt.compare(
+      data.password,
+      existingUserData.password
+    );
+    if (!validPassword) {
+      return res.status(401).json({ status: "Invalid Password" });
+    } else {
+      // Creating a jwt token
+      const token = jwt.sign({ userId: existingUserData.userId }, process.env.key, {
+        expiresIn: "1hr",
+      });
+
+      // storing token in a cookie
+      res.cookie("token", token, { httpOnly: true, maxAge: 3600000 });
+      return res.status(200).json({ status: existingUserData });
+    }
+
+  } catch (err) {
+    return res.status(404).json({ status: `Login failed ${err.msg}` });
+  }
+};
+
+
 module.exports = {
   register,
   duplicateUserIdChecker,
+  login
 };
 
--- backend/router/loginRegisterRoute.js
+++ backend/router/loginRegisterRoute.js
@@ -1,8 +1,9 @@
 const loginRegisterRouter = require("express").Router();
-const {register,duplicateUserIdChecker} = require("../controller/loginRegisterController");
+const {register,duplicateUserIdChecker, login} = require("../controller/loginRegisterController");
 
 
 loginRegisterRouter.post("/register",register);
 loginRegisterRouter.post("/duplicateUserIdChecker",duplicateUserIdChecker);
+loginRegisterRouter.post("/login",login);
 
 module.exports = loginRegisterRouter;
